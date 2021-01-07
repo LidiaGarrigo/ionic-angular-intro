bb48d70aab06ff589235d7d49c9bf106503bb6e2
= <=>
2021-01-07 21:09:09 +0100

    acabat

--- src/app/pages/nba-champions/nba-champions.page.ts
+++ src/app/pages/nba-champions/nba-champions.page.ts
@@ -7,6 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NbaChampionsPage implements OnInit {
 
+  Championships = [
+    {
+      year: 2015,
+      champion: 'Clevelands Cavaliers',
+      champion_url: '../../../assets/Cleveland.png',
+      best_players: [
+        'Lebron James',
+        'Kyrie Irving',
+        'Kevin Love',
+        'J.R. Smith',
+        'Anderson Varejao',
+      ],
+    },
+    {
+      year: 2014,
+      champion: 'Golden State Warriors',
+      champion_url: '../../../assets/GoldenState.png',
+      best_players: [
+        'Stephen curry',
+        'Klay Thompson',
+        'Draymong Green',
+        'Andrew Bogut',
+        'Gestus Ezeli',
+      ],
+    },
+    {
+      year: 2013,
+      champion: 'San Antonio Spurs',
+      champion_url: '../../../assets/SanAntonioSpurs.png',
+      best_players: [
+        'Tim Duncan',
+        'Tony Parker',
+        'Manu Ginóbili',
+        'Kawhi Leonard',
+        'Tiago Splitter',
+      ],
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
